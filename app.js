d43c71f4dcfb3d5f59173158c1b50acc14f9c5cc
Leevi <leewi.98@hotmail.com>
2023-05-08 15:10:54 +0300

    Added navigation bar to top of the screen, and as well routing for some of the buttons.

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path')
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/test', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname,"../views","login.html"));
+});
 
+router.get('/register', function(req, res, next) {
+  res.sendFile(path.join(__dirname,"../views","register.html"));
+});
 
 
 
