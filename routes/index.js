e4ad291bd3b488c13bf8e53a77cc65cd75e16972
Leevi <leewi.98@hotmail.com>
2023-05-14 22:06:22 +0300

    Modifying a lot of things to be able to render the real time python functions outputs. Added some new elements as well.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,63 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path')
+
+const { spawn } = require('child_process');
+
+let pythonProg = spawn('python',[
+  '../Play/play_in_browser.py']);
+console.log("Python program started")
+let gameIndex = 0;
+let gameStringIndex = 0;
+let dataArrived;
+pythonProg.stdout.on('data', function(data) {
+    let dataString = data.toString();
+    let dataArray = dataString.split(/\r?\n/)
+    dataArrived = true;
+    for (let index = 0; index < dataArray.length; index++) {
+      let element = dataArray[index];
+    try {
+    gameStates.push(JSON.parse(element))
+    }
+    catch {
+      if(element!="") {
+      gameProgress.push(element)
+      }
+    }
+  }
+})
+let gameProgress = [];
+let gameStates = [];
 /* GET home page. */
 router.get('/test', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/login', function(req, res, next) {
+  
+
   res.sendFile(path.join(__dirname,"../views","login.html"));
 });
+
+
+router.get("/getstate", async function(req,res,next){
+  
+  console.log("Sending game states!")
+  console.log(gameProgress)
+  gameIndex = gameStates.length;
+  gameStringIndex = gameProgress.length;
+  res.json({"gamestates": gameStates,"gameprogress": gameProgress, "gameindex":gameIndex, "dataArrived":dataArrived, "gamestringindex": gameStringIndex});
+  dataArrived = false;
+  pythonProg.stderr.on('data', function(data) {
+    console.log(`stdout: ${data}`);
+  })
+})
+
+router.post("/playmove",function(req,res,next) {
+  console.log(req.body)
+  pythonProg.stdin.write(req.body.action+"\n");
+  res.json({"Command sent":req.body.action});
+})
 
 router.get('/register', function(req, res, next) {
   res.sendFile(path.join(__dirname,"../views","register.html"));
