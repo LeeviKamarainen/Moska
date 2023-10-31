53b74d818e8c09985fd5358494b2a27f5e3433ac
Leevi <leewi.982@hotmail.com>
2023-10-31 19:17:47 +0200

    Fixed sorting for the leaderboard, and removed redundant console logs from users api.

--- public/javascripts/leaderboard.js
+++ public/javascripts/leaderboard.js
@@ -15,7 +15,7 @@ async function  initializeLeaderboard() {
     leaderboardData = await response.then(res => res.json());
     console.log(leaderboardData)
     populateLeaderboard();
-    sortTable(5); // Sort by average evaluation score by default
+    sortTable(1); // Sort by average evaluation score by default
   
 }
 
@@ -55,30 +55,70 @@ function populateLeaderboard() {
   });
 }
 
-function sortTable(columnIndex) {
-  // Get the current sorting direction for the column
-  if (!sortDirection[columnIndex]) {
-    sortDirection[columnIndex] = 'desc';
-} else if (sortDirection[columnIndex] === 'desc') {
-    sortDirection[columnIndex] = 'asc';
-} else {
-    sortDirection[columnIndex] = 'desc';
-}
+/* Help for sorting from : https://www.w3schools.com/howto/howto_js_sort_table.asp */
+function sortTable(n) {
+  var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById('leaderboard-body');;
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 0; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
 
-leaderboardData.sort((a, b) => {
-    const valueA = a[Object.keys(a)[columnIndex]];
-    const valueB = b[Object.keys(b)[columnIndex]];
-    if (typeof valueA === 'string') {
-        return (sortDirection[columnIndex] === 'asc')
-            ? valueA.localeCompare(valueB)
-            : valueB.localeCompare(valueA);
-    } else {
-        return (sortDirection[columnIndex] === 'asc')
-            ? valueA - valueB
-            : valueB - valueA;
+      // Check whether x and y are strings or numbers:
+      var xIsString = isNaN(Number(x.innerHTML));
+      var yIsString = isNaN(Number(y.innerHTML));
+      // Assume default to be numbers:
+      let xComp = Number(x.innerHTML);
+      let yComp = Number(y.innerHTML);
+      if(xIsString && yIsString) {
+        xComp = x.innerHTML.toLowerCase();
+        yComp = y.innerHTML.toLowerCase();
+      }
+      
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (xComp > yComp) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (xComp < yComp) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
     }
-});
-
-  populateLeaderboard(); // Re-render the table with sorted data
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
-  
